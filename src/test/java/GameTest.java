b34a646cf727cee31b0b845dcac33889cd79bee0
yongwoo <yongwoo8.kim@samsung.com>
2026-05-28 13:45:01 +0900

    [feature] 28

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -23,4 +23,11 @@ class GameTest {
             game.guess(null);
         });
     }
+
+    @Test
+    void throwExcptionWhenInputIsUnmatched() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            game.guess("12");
+        });
+    }
 }
