958eb83b216ae83810fbd4ca1516715515da84fc
yongwoo <yongwoo8.kim@samsung.com>
2026-05-28 13:36:07 +0900

    [refactor] 27

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,6 +1,6 @@
 public class Game {
-    public void guess(Object o) {
-        if(o==null) {
+    public void guess(Object guessNumber) {
+        if (guessNumber == null) {
             throw new IllegalArgumentException();
         }
     }
